97da3bbb87fc9c2db7f13064e962d0ab72105e1f
Kevin Murphy <kmurphy4@swarthmore.edu>
2017-06-24 12:13:46 -0400

    only define the patterns if we're going to use them

--- index.php
+++ index.php
@@ -17,29 +17,31 @@
 
   	<div class="container">
       <svg id="boardContainer" width=<?= get_setting('board_container_width') ?> height=<?= get_setting('board_container_height') ?>>
-        <defs>
-          <pattern id="lumber" x="0" y="0" patternUnites="userSpaceOnUse" width="100" height="100">
-            <image x="0" y="0" width="168" height="194" href="./resources/tile_bkg_lumber.png"> </image>
-          </pattern>
-          <pattern id="grain" x="0" y="0" patternUnites="userSpaceOnUse" width="200" height="200">
-            <image x="0" y="0" width="168" height="194" href="./resources/tile_bkg_grain.png"> </image>
-          </pattern>
-          <pattern id="wool" x="0" y="0" patternUnites="userSpaceOnUse" width="200" height="200">
-            <image x="0" y="0" width="168" height="194" href="./resources/tile_bkg_wool.png"> </image>
-          </pattern>
-          <pattern id="brick" x="0" y="0" patternUnites="userSpaceOnUse" width="200" height="200">
-            <image x="0" y="0" width="168" height="194" href="./resources/tile_bkg_brick.png"> </image>
-          </pattern>
-          <pattern id="ore" x="0" y="0" patternUnites="userSpaceOnUse" width="200" height="200">
-            <image x="0" y="0" width="168" height="194" href="./resources/tile_bkg_ore.png"> </image>
-          </pattern>
-          <pattern id="desert" x="0" y="0" patternUnites="userSpaceOnUse" width="200" height="200">
-            <image x="0" y="0" width="168" height="194" href="./resources/tile_bkg_desert.png"> </image>
-          </pattern>
-          <pattern id="ocean" x="0" y="0" patternUnites="userSpaceOnUse" width="200" height="200">
-            <image x="0" y="0" width="168" height="194" href="./resources/tile_bkg_ocean.png"> </image>
-          </pattern>
-        </defs>
+        <?php if (get_setting('background_style') == 'texture') { ?>
+          <defs>
+            <pattern id="lumber" x="0" y="0" patternUnites="userSpaceOnUse" width="100" height="100">
+              <image x="0" y="0" width="168" height="194" href="./resources/tile_bkg_lumber.png"> </image>
+            </pattern>
+            <pattern id="grain" x="0" y="0" patternUnites="userSpaceOnUse" width="200" height="200">
+              <image x="0" y="0" width="168" height="194" href="./resources/tile_bkg_grain.png"> </image>
+            </pattern>
+            <pattern id="wool" x="0" y="0" patternUnites="userSpaceOnUse" width="200" height="200">
+              <image x="0" y="0" width="168" height="194" href="./resources/tile_bkg_wool.png"> </image>
+            </pattern>
+            <pattern id="brick" x="0" y="0" patternUnites="userSpaceOnUse" width="200" height="200">
+              <image x="0" y="0" width="168" height="194" href="./resources/tile_bkg_brick.png"> </image>
+            </pattern>
+            <pattern id="ore" x="0" y="0" patternUnites="userSpaceOnUse" width="200" height="200">
+              <image x="0" y="0" width="168" height="194" href="./resources/tile_bkg_ore.png"> </image>
+            </pattern>
+            <pattern id="desert" x="0" y="0" patternUnites="userSpaceOnUse" width="200" height="200">
+              <image x="0" y="0" width="168" height="194" href="./resources/tile_bkg_desert.png"> </image>
+            </pattern>
+            <pattern id="ocean" x="0" y="0" patternUnites="userSpaceOnUse" width="200" height="200">
+              <image x="0" y="0" width="168" height="194" href="./resources/tile_bkg_ocean.png"> </image>
+            </pattern>
+          </defs>
+        <?php } ?>
 
         <?php setup_board(); ?>
 
